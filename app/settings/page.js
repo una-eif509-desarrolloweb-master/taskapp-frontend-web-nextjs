1e04401b8c492774d7548d60bc17a39928540e1e
Maikol Guzmán Alán <mikeguzman@gmail.com>
2023-10-22 21:55:53 -0600

    Data Fetching examples

--- app/settings/page.js
+++ app/settings/page.js
@@ -1,13 +1,32 @@
 import styles from './settings.module.css';
 
-export default function Page() {
+// Dynamic Data Fetching or Server Side Rendering
+// Def: https://nextjs.org/docs/pages/building-your-application/rendering/server-side-rendering
+async function fetchSettingsData() {
+    const res = await fetch(
+        "https://raw.githubusercontent.com/una-eif509-desarrolloweb-master/taskapp-frontend-web-staticdata/main/static/settings.json",
+        {cache: "no-store"}
+    );
+
+    if (!res.ok) {
+        throw new Error('Failed to fetch data')
+    }
+
+    return await res.json();
+}
+
+export default async function Page() {
+    const settingsData = await fetchSettingsData();
     return (
         <>
             <div className={styles.subContainer}>
                 <h1>Setting Page</h1>
-                <p>
-                    This is the settings page.
-                </p>
+                {settingsData.settings.map(({id, isValid, variable}) => (
+                    <div key={id} className={styles.infoContainer}>
+                        <p><span className={styles.label}>Valid: </span>{isValid.toString()}</p>
+                        <p><span className={styles.label}>Variable: </span>{variable}</p>
+                    </div>
+                ))}
             </div>
         </>
     );
--- app/support/page.js
+++ app/support/page.js
@@ -1,12 +1,31 @@
 import styles from "./support.module.css";
-export default function Page() {
+
+// Static data fetching
+// Def: https://nextjs.org/docs/app/building-your-application/data-fetching/fetching-caching-and-revalidating
+async function fetchSupportData() {
+    const res = await fetch(
+        "https://raw.githubusercontent.com/una-eif509-desarrolloweb-master/taskapp-frontend-web-staticdata/main/static/support.json"
+    );
+
+    if (!res.ok) {
+        // This will activate the closest `error.js` Error Boundary
+        throw new Error('Failed to fetch data')
+    }
+
+    return await res.json();
+}
+
+export default async function Page() {
+    const supportData = await fetchSupportData();
     return (
         <>
             <div className={styles.subContainer}>
                 <h1>Support Page</h1>
-                <p>
-                    This is the tasks page.
-                </p>
+                {supportData.support.map(({ id, data }) => (
+                    <div key={id} className={styles.infoContainer}>
+                        <p>{data}</p>
+                    </div>
+                ))}
             </div>
         </>
     );
