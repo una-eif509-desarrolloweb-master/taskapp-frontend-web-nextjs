dcb1b43991844bb4d8676fdd662c7487914a64a1
Maikol Guzmán Alán <mikeguzman@gmail.com>
2023-10-25 18:23:36 -0600

    Important updates

--- app/page.js
+++ app/page.js
@@ -1,10 +1,18 @@
-import Link from "next/link";
-
 export default function Page() {
-  return (
-      <>
-        <h1>Welcome to Globomantics App</h1>
-        <Link href="/">Home</Link>
-      </>
-  );
+    return (
+        <>
+            <h1>Welcome to TaskApp</h1>
+            <p>Welcome to TaskAPP, the ultimate learning platform for computer science students looking to master
+                ReactJS
+                with NextJS. With our intuitive interface and hands-on approach, you will be programming like a pro in
+                no
+                time.
+            </p>
+            <p>Whether you are a beginner or an experienced coder, TaskAPP is the perfect tool to take your skills to
+                the
+                next level.
+                Let's get started!</p>
+            <p>Universidad Nacional de Costa Rica</p>
+        </>
+    );
 }
