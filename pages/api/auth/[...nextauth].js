ab200cf0b36c6b852cea33629f3ac3d5070e481e
Maikol Guzmán Alán <mikeguzman@gmail.com>
2023-11-10 14:47:05 -0600

    Minor update

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,5 @@
 import NextAuth from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials";
-
-
 export const authOptions = {
     // Configure one or more authentication providers
     providers: [
